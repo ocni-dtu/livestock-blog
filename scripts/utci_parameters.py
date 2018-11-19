4ae935707ca03e95dc0c29b67765cbc3fcc1210c
ocni <ocni@dtu.dk>
2018-11-19 08:46:44 +0100

    Update documentation

---
+++ scripts/utci_parameters.py
@@ -0,0 +1,195 @@
+__author__ = "Christian Kongsgaard"
+__license__ = 'MIT'
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# IMPORTS
+
+# Modules
+import numpy as np
+import matplotlib.pyplot as plt
+from content.scripts import utci_implementations as ui
+import matplotlib.cm as cm
+import os
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Functions
+
+folder = r'C:\Users\ocni\PycharmProjects\ocni-dtu.github.io\content\images\utci_parameters'
+
+
+def mm2inch(*tuple_):
+    inch = 25.4
+    if isinstance(tuple_[0], tuple):
+        return tuple(i/inch for i in tuple_[0])
+    elif isinstance(tuple_[0], list):
+        return tuple(i/inch for i in tuple_[0])
+    else:
+        return tuple(i/inch for i in tuple_)
+
+
+def change_relhum():
+    temp = np.linspace(-30, 40, 200)
+    relhum = np.linspace(10, 100, 200)
+    t, rh = np.meshgrid(temp, relhum)
+    mrt = t
+    wind = np.ones((200, 200)) * 3
+
+    utci = ui.utci_numpy(t, mrt, wind, rh)
+
+    return t, rh, utci
+
+
+def plot3d_relhum():
+    temp, relhum, utci = change_relhum()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(temp, relhum, utci, cmap='afmhot')
+    ax.set_ylabel('Relative Humidity')
+    ax.set_xlabel('Air Temperature')
+    ax.set_zlabel('UTCI')
+    plt.show()
+
+
+#plot3d_relhum()
+
+
+def change_wind():
+    temp = np.linspace(-30, 40, 200)
+    wind = np.linspace(0.5, 17, 200)
+    relhum = np.ones((200, 200)) * 50
+    t, wi = np.meshgrid(temp, wind)
+    mrt = t
+    utci = ui.utci_numpy(t, mrt, wi, relhum)
+
+    return t, wi, utci
+
+
+def plot3d_wind():
+    temp, wind, utci = change_wind()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(temp, wind, utci, cmap='afmhot')
+    ax.set_ylabel('Wind Speed')
+    ax.set_xlabel('Air Temperature')
+    ax.set_zlabel('UTCI')
+    plt.show()
+
+
+#change_wind()
+
+
+def change_mrt():
+    temp = np.linspace(-30, 40, 200)
+    wind = np.ones((200, 200)) * 3
+    relhum = np.ones((200, 200)) * 50
+    mrt = np.linspace(-50, 70, 200)
+    t, mt = np.meshgrid(temp, mrt)
+    utci = ui.utci_numpy(t, mt, wind, relhum)
+
+    return t, mt, utci
+
+
+def plot3d_mrt():
+    temp, mrt, utci = change_mrt()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(temp, mrt, utci, cmap='afmhot')
+    ax.set_ylabel('Mean Radiant Temperature')
+    ax.set_xlabel('Air Temperature')
+    ax.set_zlabel('UTCI')
+    plt.show()
+
+
+#change_mrt()
+
+
+def standard_utci():
+    temp = np.linspace(-30, 40, 200)
+    wind = np.ones((200, 200)) * 3
+    relhum = np.ones((200, 200)) * 50
+    t, mt = np.meshgrid(temp, temp)
+    mrt = t
+    utci = ui.utci_numpy(t, mrt, wind, relhum)
+
+    return utci
+
+
+def relhum_difference(save=False):
+    temp, relhum, utci_rh = change_relhum()
+    utci_std = standard_utci()
+
+    diff_utci = utci_rh - utci_std
+
+    colors = cm.RdBu_r
+    fig, ax = plt.subplots(1, 1, figsize=mm2inch(250, 200))
+    plot = ax.pcolor(temp, relhum, diff_utci, cmap=colors, vmin=-35, vmax=35)
+    levels = [-2, -1, 0, 1, 2, 5, 10, 15, 20, 30]
+    con = ax.contour(temp, relhum, diff_utci, levels, colors='k')
+    plt.clabel(con, inline=1, fontsize=10, fmt='%d')
+    ax.set_xlabel('Air Temperature [°C]')
+    ax.set_ylabel('Relative Humidity [%]')
+    bar = fig.colorbar(plot, ax=ax)
+    bar.set_label('UTCI Difference [°C]')
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(os.path.join(folder, 'utci_relhum.png'), dpi=300)
+
+    plt.show()
+
+
+#relhum_difference(True)
+
+
+def wind_difference(save=False):
+    temp, wind, utci_wind = change_wind()
+    utci_std = standard_utci()
+
+    diff_utci = utci_wind - utci_std
+
+    colors = cm.RdBu_r
+    fig, ax = plt.subplots(1, 1, figsize=mm2inch(250, 200))
+    plot = ax.pcolor(temp, wind, diff_utci, cmap=colors, vmin=-35, vmax=35)
+    levels = [-30, -25, -20, -15, -10, -5, 0, 5, 10]
+    con = ax.contour(temp, wind, diff_utci, levels, colors='k')
+    plt.clabel(con, levels, inline=1, fontsize=10, fmt='%d')
+    ax.set_xlabel('Air Temperature [°C]')
+    ax.set_ylabel('Wind Speed [m/s]')
+    bar = fig.colorbar(plot, ax=ax)
+    bar.set_label('UTCI Difference [°C]')
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(os.path.join(folder, 'utci_wind.png'), dpi=300)
+
+    plt.show()
+
+
+#wind_difference(True)
+
+
+def mrt_difference(save=False):
+    temp, mrt, utci_mrt = change_mrt()
+    utci_std = standard_utci()
+
+    diff_utci = utci_mrt - utci_std
+
+    colors = cm.RdBu_r
+    fig, ax = plt.subplots(1, 1, figsize=mm2inch(250, 200))
+    plot = ax.pcolor(temp, mrt, diff_utci, cmap=colors, vmin=-35, vmax=35)
+    levels = [-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
+    con = ax.contour(temp, mrt, diff_utci, levels, colors='k')
+    plt.clabel(con, inline=1, fontsize=10, fmt='%d')
+    ax.set_xlabel('Air Temperature [°C]')
+    ax.set_ylabel('Mean Radiant Temperature [°C]')
+    bar = fig.colorbar(plot, ax=ax)
+    bar.set_label('UTCI Difference [°C]')
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(os.path.join(folder, 'utci_mrt.png'), dpi=300)
+
+    plt.show()
+
+
+mrt_difference()
---
+++ scripts/utci_speed.py
@@ -0,0 +1,127 @@
+__author__ = "Christian Kongsgaard"
+__license__ = 'MIT'
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# IMPORTS
+
+# Modules
+import numpy as np
+import matplotlib.pyplot as plt
+from pyepw.epw import EPW
+import time
+
+from content.scripts.utci_implementations import *
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# UTCI Speed Tests
+
+epw = EPW()
+try:
+    epw_path = r'C:\Users\ocni\Dropbox\Uddannelse\DTU\Diverse\EPW_WeatherData\DNK_Copenhagen.061800_IWEC.epw'
+    epw.read(epw_path)
+except FileNotFoundError:
+    epw_path = r'C:\Users\Christian\Dropbox\Uddannelse\DTU\Diverse\EPW_WeatherData\DNK_Copenhagen.061800_IWEC.epw'
+    epw.read(epw_path)
+
+air_temp = []
+rel_hum = []
+wind = []
+
+for weather_data in epw.weatherdata:
+    air_temp.append(weather_data.dry_bulb_temperature)
+    rel_hum.append(weather_data.relative_humidity)
+    wind.append(weather_data.wind_speed)
+
+mrt_temp = air_temp
+
+
+def time_slow_utci(years=1):
+    air_dub = []
+    mrt_dub = []
+    wind_dub = []
+    rh_dub = []
+
+    for y in range(years):
+        air_dub.extend(air_temp)
+        mrt_dub.extend(mrt_temp)
+        wind_dub.extend(wind)
+        rh_dub.extend(rel_hum)
+
+    t0 = time.time()
+    for i in range(len(air_dub)):
+        utci_ladybug(air_dub[i], mrt_dub[i], wind_dub[i], rh_dub[i])
+    return time.time()-t0
+
+
+def time_numpy_utci(years=1):
+    air_dub = []
+    mrt_dub = []
+    wind_dub = []
+    rh_dub = []
+
+    for y in range(years):
+        air_dub.extend(air_temp)
+        mrt_dub.extend(mrt_temp)
+        wind_dub.extend(wind)
+        rh_dub.extend(rel_hum)
+
+    air_np = np.array(air_dub)
+    rh_np = np.array(rh_dub)
+    wind_np = np.array(wind_dub)
+    mrt_np = np.array(mrt_dub)
+
+    t0 = time.time()
+    utci_numpy(air_np, mrt_np, wind_np, rh_np)
+
+    return time.time() - t0
+
+
+numpy_time = [time_numpy_utci(i)
+              for i in range(10)]
+
+slow_time = [time_slow_utci(i)
+             for i in range(10)]
+
+colors = ['#F44336', '#FFBF1F', '#57DA10', '#12B2FF', '#7D1BDA']
+
+hours = [(h * 8760)
+         for h in range(1, 11)]
+
+plt.figure(figsize=(12, 6.75))
+plt.title('UTCI Computation Time Comparison\nCPython')
+plt.plot(hours, numpy_time, label='NumPy', color=colors[0])
+plt.plot(hours, slow_time, label='Original', color=colors[1])
+plt.legend()
+plt.ylabel('Time in seconds')
+plt.xlabel('Number of hours computed')
+plt.xlim(0, 90000)
+plt.tight_layout()
+
+ladybug_item_time = [6.7, 9.0, 13.8, 17.5, 21.6, 30, 37.5, 38.3, 39.0, 50.0]
+ladybug_list_time = [1.1, 2.0, 3.3, 4.5, 5.5, 6.6, 7.6, 8.8, 10.0, 11.2]
+ladybug_tree_time = [0.995, 1.9, 2.9, 4.0, 4.9, 5.8, 6.9, 8.0, 8.7, 10.4]
+livestock_time = [0.35, 0.535, 0.629, 0.747, 0.891, 1.1, 1.3, 1.3, 1.5, 1.6]
+
+plt.figure(figsize=(12, 6.75))
+plt.title('UTCI Computation Time Comparison\nGrasshopper Component')
+plt.plot(hours, ladybug_list_time, label='List Inputs', color=colors[2])
+plt.plot(hours, ladybug_tree_time, label='Tree Inputs', color=colors[3])
+plt.plot(hours, livestock_time, label='Livestock', color=colors[0])
+plt.legend()
+plt.ylabel('Time in seconds')
+plt.xlabel('Number of hours computed')
+plt.xlim(0, 90000)
+plt.tight_layout()
+
+plt.figure(figsize=(12, 6.75))
+plt.title('UTCI Computation Time Comparison\nGrasshopper Component')
+plt.plot(hours, ladybug_item_time, label='Item Inputs', color=colors[1])
+plt.plot(hours, ladybug_list_time, label='List Inputs', color=colors[2])
+plt.plot(hours, ladybug_tree_time, label='Tree Inputs', color=colors[3])
+plt.legend()
+plt.ylabel('Time in seconds')
+plt.xlabel('Number of hours computed')
+plt.xlim(0, 90000)
+plt.tight_layout()
+
+plt.show()
